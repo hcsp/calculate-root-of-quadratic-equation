6f802e78211a70c1ea5398644a2fd61dec038d39
jamescao <James.Magicbook@outlook.com>
2019-11-01 01:06:47 +0800

    Java的运算系统练习：求一元二次方程的根

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -16,17 +16,19 @@ public class Solution {
     // 提示，你可利用求根公式x=[-b±√(b²-4ac)]/2a
     // 需要执行开方运算时可使用Math.sqrt()方法
     public static double[] calculate(int a, int b, int c) {
-        double f = Math.sqrt(b * b - 4 * a * c);
 
-        if (f > 0) {
-            double root1 = (-b + f) / 2 * a;
-            double root2 = (-b - f) / 2 * a;
+        double d = b * b - 4 * a * c;
+
+        if (d > 0) {
+            double root1 = (-b + Math.sqrt(d)) / 2 * a;
+            double root2 = (-b - Math.sqrt(d)) / 2 * a;
             return new double[] { root1, root2 };
-        } else if (f < 0) {
+        } else if (d < 0) {
             return new double[] {};
         } else {
             double root = -b / 2 * a;
             return new double[] { root };
         }
+
     }
 }
