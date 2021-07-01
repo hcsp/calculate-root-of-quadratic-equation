aef52a5777f2fb0d13a8ca85ffc9c9bda078422c
skyszhang <1296003529@qq.com>
2021-07-01 14:48:33 +0800

    calculate root of quadratic equation

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -15,5 +15,17 @@ public class Solution {
     // 若没有根，返回一个空数组，即：new double[] {}
     // 提示，你可利用求根公式x=[-b±√(b²-4ac)]/2a
     // 需要执行开方运算时可使用Math.sqrt()方法
-    public static double[] calculate(int a, int b, int c) {}
+    public static double[] calculate(int a, int b, int c) {
+        double[] x = new double[2];
+        if (Math.sqrt(b * b - 4 * a * c) > 0) {
+            x[0] = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+            x[1] = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+        } else if (Math.sqrt(b * b - 4 * a * c) == 0) {
+            x[0] = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+        } else if (Math.sqrt(b * b - 4 * a * c) < 0) {
+            return x;
+        }
+        return x;
+    }
 }
+
