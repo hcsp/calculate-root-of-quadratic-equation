7e1c2de2601c4e2d69402efc2631f4caf0db09bb
lee <lindamoxiaobei@163.com>
2021-10-18 22:17:08 +0100

    先判断 delat，然后再去求根的值；

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -1,12 +1,14 @@
 package com.github.hcsp.calculation;
 
 import java.util.Arrays;
+import java.util.Currency;
 
 public class Solution {
     public static void main(String[] args) {
         System.out.println("x^2-2x+1=0的解是：" + Arrays.toString(calculate(1, -2, 1)));
         System.out.println("x^2-4=0的解是：" + Arrays.toString(calculate(1, 0, -4)));
         System.out.println("x^2+1=0的解是：" + Arrays.toString(calculate(1, 0, 1)));
+
     }
 
     // 计算一元二次方程ax^2+bx+c=0的根。
@@ -16,19 +18,22 @@ public class Solution {
     // 提示，你可利用求根公式x=[-b±√(b²-4ac)]/2a
     // 需要执行开方运算时可使用Math.sqrt()方法
     public static double[] calculate(int a, int b, int c) {
-        double root1 =  (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
-        double root2 =  (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
 
-
-        Boolean result = root1 == root2;
-        if (result == null) {
+        double delta =  Math.sqrt(b * b - 4 * a * c);
+        if (delta < 0) {
             return new double[]{};
-        } else if (result) {
+        } else if (delta == 0) {
+
+            double root1 =  (-b +delta) / (2 * a);
             return new double[]{root1};
-        } else
-            return new double[]{root1, root2};
-        // 利用三元运算符来精简if-else 判断运算符真的好香啊
-//       return  result == null ? (new double[] {}) : (result ? new double[] {root1} : new double[]{root1 , root2});
+        } else {
+            double root1 =  (-b +delta) / (2 * a);
+
+            double root2 =  (-b -delta) / (2 * a);
+
+            return new double[]{root1,root2};
+        }
+
     }
 }
 
